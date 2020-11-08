62caa0aed2132110f3de85d43039cef85b441998
Alex Shavrin <alex.shavrin200303mail.ru>
2020-11-08 21:36:34 +0300

    Ввел проверку на нажатие кнопок Esc и задал fps

--- main.py
+++ main.py
@@ -7,6 +7,9 @@ window_width = 400
 window_height = 400
 window_title = 'Snake'
 window_icon = pygame.image.load('images/Icon.png')
+window_fps = 5
+
+clock = pygame.time.Clock()
 
 pygame.display.set_mode((window_width, window_height))
 pygame.display.set_caption(window_title)
@@ -18,4 +21,8 @@ while True:
         if event.type == pygame.QUIT:
             sys.exit()
 
+    keys = pygame.key.get_pressed()
+    if keys[pygame.K_ESCAPE]:
+        sys.exit()
 
+    clock.tick(window_fps)
