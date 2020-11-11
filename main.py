3592976f2c54f54306ee9d75879da2225b4574a6
Alex Shavrin <alex.shavrin200303mail.ru>
2020-11-11 16:24:36 +0300

    Произведен рефакторинг кода - описаны функции документации
    В файле main.py были добавлены функции:
    food_check_collision
    food_set_pos
    snake_move
    snake_check_collision
    
    В функции check_events теперь меняется не координаты змейки,
    а направление её движения
    
    В модуле parameters.py добавлена переменная
    snake_direction - отвечает за направление змейки
    (по умолчанию 'up')

--- main.py
+++ main.py
@@ -4,7 +4,8 @@ from parameters import *
 
 
 def check_events():
-    global snake_pos_x, snake_pos_y
+    """Проверка событий программы и клавиатуры"""
+    global snake_direction
 
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
@@ -13,16 +14,17 @@ def check_events():
     keys = pygame.key.get_pressed()
     if keys[pygame.K_ESCAPE]:
         sys.exit()
-    if keys[pygame.K_w] and snake_pos_y > 0:
-        snake_pos_y -= window_pixel
-    elif keys[pygame.K_s] and snake_pos_y + shake_height < window_height:
-        snake_pos_y += window_pixel
-    elif keys[pygame.K_a] and snake_pos_x > 0:
-        snake_pos_x -= window_pixel
-    elif keys[pygame.K_d] and snake_pos_x + snake_width < window_width:
-        snake_pos_x += window_pixel
+    if keys[pygame.K_w]:
+        snake_direction = 'up'
+    elif keys[pygame.K_s]:
+        snake_direction = 'down'
+    elif keys[pygame.K_a]:
+        snake_direction = 'left'
+    elif keys[pygame.K_d]:
+        snake_direction = 'right'
 
 def draw_fon():
+    """Рисуем фон окна"""
     window.fill(color_fon)
 
     for y in range(window_pixel, window_height, window_pixel):
@@ -31,15 +33,59 @@ def draw_fon():
         pygame.draw.line(window, color_grid, (x, 0), (x, window_height))
 
 def food_draw():
+    """Рисуем еду"""
     pygame.draw.rect(window, color_food, (food_pos_x, food_pos_y, food_width, food_height))
 
+def food_check_collision():
+    """Проверка на пересечение координат змейки и еды"""
+    global window_fps
+
+    if snake_pos_x == food_pos_x and snake_pos_y == food_pos_y:
+        food_set_pos()
+        window_fps += 1
+
+def food_set_pos():
+    """Устанавливаем новые координаты для еды"""
+    global food_pos_x, food_pos_y
+
+    food_pos_x = random.randrange(0, window_width, window_pixel)
+    food_pos_y = random.randrange(0, window_height, window_pixel)
+
 def snake_draw():
+    """Рисуем змейку"""
     pygame.draw.rect(window, color_snake, (snake_pos_x, snake_pos_y, snake_width, shake_height))
+
+def snake_move():
+    """Отвечает за передвижение змейки"""
+    global snake_pos_x, snake_pos_y
+
+    if snake_direction == 'up':
+        snake_pos_y -= window_pixel
+    elif snake_direction == 'down':
+        snake_pos_y += window_pixel
+    elif snake_direction == 'left':
+        snake_pos_x -= window_pixel
+    else:
+        snake_pos_x += window_pixel
+
+def snake_check_collision():
+    """Проверяем на столкновение змейки с стенкой"""
+    global snake_pos_x, snake_pos_y
+    if snake_pos_y < 0:
+        snake_pos_y = window_height - window_pixel
+    elif snake_pos_y > window_height - window_pixel:
+        snake_pos_y = 0
+    if snake_pos_x < 0:
+        snake_pos_x = window_width - window_pixel
+    elif snake_pos_x > window_width - window_pixel:
+        snake_pos_x = 0
+
 
 pygame.init()
 
 clock = pygame.time.Clock()
 
+# Создаем окно программы
 window = pygame.display.set_mode((window_width, window_height))
 pygame.display.set_caption(window_title)
 pygame.display.set_icon(window_icon)
@@ -47,7 +93,11 @@ pygame.display.set_icon(window_icon)
 while True:
     draw_fon()
     check_events()
+    food_check_collision()
     food_draw()
+    snake_check_collision()
+    snake_move()
     snake_draw()
     pygame.display.update()
+
     clock.tick(window_fps)
--- parameters.py
+++ parameters.py
@@ -2,6 +2,7 @@ import random
 import pygame
 pygame.init()
 
+# Настройки параметров экрана
 window_width = 500
 window_height = 500
 window_title = 'Snake'
@@ -9,17 +10,22 @@ window_icon = pygame.image.load('images/Icon.png')
 window_fps = 5
 window_pixel = 25
 
+# Настройки цветов
 color_snake = (10, 200, 10)
 color_fon = (0, 0, 0)
 color_grid = (60, 60, 60)
 color_food = (150, 10, 10)
 
-snake_pos_x = 0
-snake_pos_y = 0
+# Настройки параметров змейки
+snake_pos_x = window_width // 2
+snake_pos_y = window_height // 2
 snake_width = window_pixel
 shake_height = window_pixel
+snake_direction = 'up'
 
+# Настройки параметров еды
 food_width = window_pixel
 food_height = window_pixel
-food_pos_x = random.randrange(0, window_width - food_width, window_pixel)
-food_pos_y = random.randrange(0, window_height - food_height, window_pixel)
+food_pos_x = random.randrange(0, window_width, window_pixel)
+food_pos_y = random.randrange(0, window_height, window_pixel)
+
