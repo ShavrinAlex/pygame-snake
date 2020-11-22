b8edd942666c4f8b54466ab997280fd822c940cf
Alex Shavrin <alex.shavrin200303mail.ru>
2020-11-22 20:29:06 +0300

    Созданы классы: App(), Snake(), Food().

--- main.py
+++ main.py
@@ -2,3 +2,25 @@ import time
 import random
 import pygame as pg
 
+class App:
+    """
+    Управляет всеми процессами приложения.
+    """
+
+    pass
+
+
+class Snake:
+    """
+    Класс змейки.
+    """
+
+    pass
+
+
+class Food:
+    """
+    Класс еды.
+    """
+
+    pass
